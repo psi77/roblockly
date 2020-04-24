ea0c429c7f5bc32d27f890e2ed2544356bb000e6
psi77 <simon@smith77.co.uk>
2020-04-25 00:17:49 +0100

    Better way of loading toolbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,6 +34,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    HttpClientModule,
 
     MatCardModule,
     MatButtonModule,
@@ -49,7 +51,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  schemas: [NO_ERRORS_SCHEMA]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import * as Blockly from 'blockly';
 
 @Component({
@@ -10,25 +11,42 @@ export class EditorComponent implements OnInit {
 
   workspace: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
+    // const xmlDoc = document.implementation.createDocument('', '', null);
+    // const root = xmlDoc.createElement('xml');
+    // const b1 = xmlDoc.createElement('block');
+    // b1.setAttribute('type', 'controls_if');
+    // root.appendChild(b1);
+    // then use root.outerHTML
+
+    // TODO: cache this somewhere so we only load it once
+    this.http.get(
+      'assets/toolbox.xml',
+      {
+        responseType: 'text'
+      }
+    ).subscribe(this.postInit);
+  }
+
+  postInit(toolboxData: any) {
     this.workspace = Blockly.inject(
       document.getElementById('blocklyDiv'),
       {
         readOnly: false,
-        // media: 'media/',
+        media: 'media/',
         trashcan: true,
         move: {
           scrollbars: true,
           drag: true,
           wheel: true
         },
-        toolbox: document.getElementById('toolbox')
+        toolbox: toolboxData
       } as Blockly.BlocklyOptions
     );
   }
-
 
   saveProgram(): void {
     // this.program.xmlData = Blockly.Xml.domToText(
