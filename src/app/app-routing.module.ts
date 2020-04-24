46ba3ce1e7f5eb7e725435251fd013b5ce7b4345
psi77 <simon@smith77.co.uk>
2020-04-24 21:54:19 +0100

    Working phaser in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './game/game.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'game', component: GameComponent },
+  { path: '', redirectTo: '/game', pathMatch: 'full' },
+  { path: '**', component: GameComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser';
+
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'main' });
+  } create() {
+    console.log('create method');
+  } preload() {
+    console.log('preload method');
+  } update() {
+    console.log('update method');
+  }
+}
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+  phaserGame: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig; constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      height: 600,
+      width: 800,
+      scene: [MainScene],
+      parent: 'gameContainer',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 100 }
+        }
+      }
+    };
+  } ngOnInit() {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+}
