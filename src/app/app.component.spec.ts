88d3d167f6a3e6c006236a329fd10a235966cfda
psi77 <simon@smith77.co.uk>
2020-05-08 22:43:57 +0100

    RoBlockly

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-phaser-blockly'`, () => {
+  it(`should have as title 'RoBlockly'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-phaser-blockly');
+    expect(app.title).toEqual('RoBlockly');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-phaser-blockly!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to RoBlockly!');
   });
 });
